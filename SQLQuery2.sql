f5bcfac50a1947a71c06d16351033716eb459ec1
Hadassa Juliane <hadassajuliane405@gmail.com>
2026-09-11 11:07:48 -0300

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,51 @@
+--CREATE DATABASE Atividade;
+--USE Atividade;
+--CREATE TABLE EMPREGADO( 
+--           Mt_Empregado    INT              IDENTITY(1001,1)   PRIMARY KEY,
+--		   Nome            VARCHAR(50)      NOT NULL,
+--		   Sexo            CHAR(1)          NOT NULL           CHECK (sexo in ('M', 'F')));
+
+--CREATE TABLE DEPENDENTE(
+--           Cd_Dependente    INT              IDENTITY(100,1)    PRIMARY KEY,
+--		   Nome             VARCHAR(50)      NOT NULL,
+--		   Sexo             CHAR(1)          NOT NULL           CHECK (Sexo in ('F', 'M')),
+--		   Mt_Empregado     INT              NOT NULL           REFERENCES Empregado(Mt_Empregado) 
+--		   ON DELETE CASCADE);
+
+--INSERT INTO EMPREGADO (Nome, Sexo)
+--VALUES
+--	('Fred', 'M'),
+--	('Bianca', 'F');
+--INSERT INTO DEPENDENTE (Nome, Sexo, Mt_Empregado)
+--VALUES
+--	('Yasmin', 'M', 1001),
+--	('Lana', 'F', 1002);
+
+--DROP TABLE DEPENDENTE;
+--DROP TABLE EMPREGADO;
+
+
+--CREATE TABLE EMPREGADO( 
+--           Mt_Empregado    INT              IDENTITY(1001,1)   PRIMARY KEY,
+--		   Nome            VARCHAR(50)      NOT NULL,
+--		   Sexo            CHAR(1)          NOT NULL           CHECK (sexo in ('M', 'F')));
+
+--CREATE TABLE DEPENDENTE(
+--           Cd_Dependente    INT              IDENTITY(100,1)    PRIMARY KEY,
+--		   Nome             VARCHAR(50)      NOT NULL,
+--		   Sexo             CHAR(1)          NOT NULL           CHECK (Sexo in ('F', 'M')),
+--		   Mt_Empregado     INT              NOT NULL           REFERENCES Empregado(Mt_Empregado) 
+--		   ON DELETE CASCADE);
+
+--INSERT INTO EMPREGADO (Nome, Sexo)
+--VALUES
+--	('Fred', 'M'),
+--	('Bianca', 'F');
+--INSERT INTO DEPENDENTE (Nome, Sexo, Mt_Empregado)
+--VALUES
+--	('Yasmin', 'M', 1001),
+--	('Lana', 'F', 1002);
+
+--DELETE FROM EMPREGADO WHERE Mt_Empregado = 1001
+--select*from Empregado
+--select*from Dependente
